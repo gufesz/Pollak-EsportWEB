0512546f94e1772f9663ccbd1b2a13bb93f2b711
mor <tmor553@gmail.com>
2025-01-31 10:28:04 +0100

    Add files via upload

---
+++ esport/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector(".content-button-main");
+
+    if (button) {
+        button.addEventListener("click", function () {
+            window.location.href = "home.html";
+        });
+    }
+});
